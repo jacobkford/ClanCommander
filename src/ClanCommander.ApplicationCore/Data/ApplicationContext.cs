499146d32385fd63b187b0184d1aaf9840c17da5
Jacob Ford <jaycub.k.ford@gmail.com>
2021-12-29 12:39:24 +0000

    feat: added entityframework core with database access functionality

---
+++ src/ClanCommander.ApplicationCore/Data/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿namespace ClanCommander.ApplicationCore.Data;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
--- src/ClanCommander.ApplicationCore/ServiceRegistration.cs
+++ src/ClanCommander.ApplicationCore/ServiceRegistration.cs
@@ -1,20 +1,18 @@
-﻿using ClanCommander.ApplicationCore.Services;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace ClanCommander.ApplicationCore;
+﻿namespace ClanCommander.ApplicationCore;
 
 public static class ServiceRegistration
 {
     public static IServiceCollection AddApplicationCore(this IServiceCollection services, IConfiguration configuration)
     {
-        // services.AddScoped<MailService>();
+        services.AddDbContext<ApplicationContext>(options 
+            => options.UseSqlite(configuration.GetConnectionString("ClanCommanderSqlite")));
+
         services.AddClashOfClans(options =>
         {
             options.Tokens.Add(configuration["ClashOfClans:Token"]);
         });
 
-        services.AddScoped<IMailService, MailService>();
+        // services.AddScoped<IMailService, MailService>();
         services.AddScoped<IClashOfClansService, ClashOfClansService>();
 
         return services;
