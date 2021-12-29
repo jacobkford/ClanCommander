f27c930e023181722e8f7e3c207badf18e3fa45b
Jacob Ford <jaycub.k.ford@gmail.com>
2021-12-29 12:37:40 +0000

    refactor: added global usings files

---
+++ src/ClanCommander.ApplicationCore/GlobalUsings.cs
@@ -0,0 +1,34 @@
+﻿global using ClanCommander.ApplicationCore.Extensions;
+global using ClanCommander.ApplicationCore.Entities;
+global using ClanCommander.ApplicationCore.Data;
+global using ClanCommander.ApplicationCore.Interfaces;
+global using ClanCommander.ApplicationCore.Services;
+
+global using ClanCommander.SharedKernal.Interfaces;
+global using ClanCommander.SharedKernel.Models;
+
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using Discord;
+global using Discord.Commands;
+global using Discord.Interactions;
+global using Discord.WebSocket;
+
+global using ClashOfClans;
+global using ClashOfClans.Models;
+global using ClanData = ClashOfClans.Models.Clan;
+
+global using Google.Apis.Auth.OAuth2;
+global using Google.Apis.Gmail.v1;
+global using Google.Apis.Services;
+global using Google.Apis.Util.Store;
+
+global using Ardalis.GuardClauses;
---
+++ src/ClanCommander.DiscordBot/GlobalUsings.cs
@@ -0,0 +1,31 @@
+﻿global using ClanCommander.DiscordBot.Services;
+
+global using ClanCommander.ApplicationCore;
+global using ClanCommander.ApplicationCore.Extensions;
+global using ClanCommander.ApplicationCore.Entities;
+global using ClanCommander.ApplicationCore.Data;
+global using ClanCommander.ApplicationCore.Interfaces;
+global using ClanCommander.ApplicationCore.Services;
+
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Reflection;
+
+global using Serilog;
+global using Serilog.Events;
+
+global using Discord;
+global using Discord.Commands;
+global using Discord.Interactions;
+global using Discord.WebSocket;
+global using Discord.Addons.Hosting;
+global using Discord.Addons.Hosting.Util;
+global using ICommandResult = Discord.Commands.IResult;
+global using IInteractionResult = Discord.Interactions.IResult;
+
+global using Fergun.Interactive;
+
+
