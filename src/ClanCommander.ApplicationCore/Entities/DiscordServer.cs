da699ff085f40121a15d9e6d003e58ef303416e7
Jacob Ford <jaycub.k.ford@gmail.com>
2021-12-29 12:45:55 +0000

    feat: added discord server entity with tests

---
+++ src/ClanCommander.ApplicationCore/Entities/DiscordServer.cs
@@ -0,0 +1,47 @@
+﻿namespace ClanCommander.ApplicationCore.Entities;
+
+internal class DiscordServer : Entity, IAggregateRoot
+{
+    public ulong Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string? Prefix { get; private set; }
+
+    public bool MessageCommandsEnabled { get; private set; }
+
+    private DiscordServer() { }
+
+    public DiscordServer(ulong id, string name)
+    {
+        Id = Guard.Against.InvalidDiscordSnowflakeId(id, nameof(id));
+        Name = Guard.Against.NullOrWhiteSpace(name, nameof(name));
+        Prefix = null;
+        MessageCommandsEnabled = false;
+    }
+
+    public Clan CreateServerClan(string id, string name)
+    {
+        Guard.Against.NullOrWhiteSpace(id, nameof(id));
+        Guard.Against.NullOrWhiteSpace(name, nameof(name));
+
+        return new Clan(id, name, Id);
+    }
+
+    public void UpdatePrefix(string prefix)
+    {
+        Prefix = Guard.Against.NullOrWhiteSpace(prefix, nameof(prefix));
+    }
+
+    public void EnableMessageCommands()
+    {
+        Guard.Against.NullOrWhiteSpace(Prefix, nameof(Prefix));
+        MessageCommandsEnabled = true;
+    }
+
+    public void DisableMessageCommands()
+    {
+        MessageCommandsEnabled = false;
+    }
+}
+
---
+++ tests/ClanCommander.UnitTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using Xunit;
+global using FluentAssertions;
+global using static FluentAssertions.FluentActions;
+global using Moq;
+
+global using System;
+global using System.Collections.Generic;
+
+global using ClanCommander.ApplicationCore.Entities;
