d3b78be71e276214747c2023e3c6f8073c144162
Jacob Ford <jaycub.k.ford@gmail.com>
2022-02-22 16:33:54 +0000

    feat: added Clash Of Clans clan role smart enum

--- src/ClanCommander.ApplicationCore/GlobalUsings.cs
+++ src/ClanCommander.ApplicationCore/GlobalUsings.cs
@@ -1,12 +1,23 @@
 ﻿global using ClanCommander.ApplicationCore.Extensions;
-global using ClanCommander.ApplicationCore.Entities;
-global using ClanCommander.ApplicationCore.Entities.DiscordServerAggregate;
-global using ClanCommander.ApplicationCore.Entities.ClanAggregate;
-global using ClanCommander.ApplicationCore.Entities.UserAggregate;
+global using ClanCommander.ApplicationCore.Entities.Shared;
+global using ClanCommander.ApplicationCore.Entities.Guild;
+global using ClanCommander.ApplicationCore.Entities.Guild.Events;
+global using ClanCommander.ApplicationCore.Entities.MessageCommands;
+global using ClanCommander.ApplicationCore.Entities.ClashOfClans;
+global using ClanCommander.ApplicationCore.Entities.ClashOfClans.Events;
+global using ClanCommander.ApplicationCore.Entities.Users;
+global using ClanCommander.ApplicationCore.Entities.Users.Events;
+global using ClanCommander.ApplicationCore.Entities.GuildEntry.ClashOfClans;
+global using ClanCommander.ApplicationCore.Entities.GuildEntry.ClashOfClans.Events;
+global using ClanCommander.ApplicationCore.Entities.GuildEntry.ClashOfClans.Interview.Events;
+global using ClanCommander.ApplicationCore.Entities.Logging;
+global using ClanCommander.ApplicationCore.Entities.Greeting;
 global using ClanCommander.ApplicationCore.Data;
+global using ClanCommander.ApplicationCore.Data.ValueConverters;
 global using ClanCommander.ApplicationCore.Interfaces;
 global using ClanCommander.ApplicationCore.Services;
 global using ClanCommander.ApplicationCore.Constants;
+global using ClanCommander.ApplicationCore.Exceptions;
 
 global using ClanCommander.SharedKernal.Interfaces;
 global using ClanCommander.SharedKernel.Models;
@@ -15,6 +26,7 @@ global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
+global using System.Linq.Expressions;
 global using System.Threading.Tasks;
 global using System.Text.RegularExpressions;
 
@@ -22,6 +34,7 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 global using Discord;
 global using Discord.Commands;
@@ -38,3 +51,5 @@ global using Google.Apis.Util.Store;
 
 global using Ardalis.GuardClauses;
 global using Ardalis.SmartEnum;
+
+global using MediatR;
