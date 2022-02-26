a41015d458900ac9d5990d64e5b5370f7fb74ec4
Jacob Ford <jaycub.k.ford@gmail.com>
2022-02-26 01:09:08 +0000

    feat: added dbcontext factory for building migrations & updating db

---
+++ src/ClanCommander.ApplicationCore/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace ClanCommander.ApplicationCore.Data;
+
+internal class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("PostgreSQL");
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+
+    }
+}
