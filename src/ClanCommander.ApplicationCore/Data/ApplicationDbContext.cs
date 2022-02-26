12b97245be00d1c2a21f525754b2e467ce4f8d10
Jacob Ford <jaycub.k.ford@gmail.com>
2022-02-26 01:08:03 +0000

    feat: configured dbcontext with postgresql

---
+++ src/ClanCommander.ApplicationCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace ClanCommander.ApplicationCore.Data;
+
+internal class ApplicationDbContext : DbContext
+{
+    #region Discord Context
+    public const string DISCORD_SCHEMA = "discord";
+    public DbSet<RegisteredDiscordGuild> DiscordGuilds { get; set; }
+    public DbSet<GuildMessageCommands> MessageCommandsConfigurations { get; set; }
+    public DbSet<DiscordUser> DiscordUsers { get; set; }
+    #endregion
+
+    #region Discord Clash Of Clans Context
+    public const string DISCORDCLASHOFCLANS_SCHEMA = "discord_coc";
+    public DbSet<GuildClan> GuildClans { get; set; }
+    public DbSet<GuildClanMember> GuildClanMembers { get; set; }
+    public DbSet<GuildClanMemberRole> GuildClanMemberRoles { get; set; }
+    public DbSet<ClashOfClansAccount> ClashOfClansUserAccounts { get; set; }
+    #endregion
+
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
--- src/ClanCommander.ApplicationCore/ServiceRegistration.cs
+++ src/ClanCommander.ApplicationCore/ServiceRegistration.cs
@@ -1,21 +1,19 @@
-﻿namespace ClanCommander.ApplicationCore;
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ClanCommander.ApplicationCore;
 
 public static class ServiceRegistration
 {
     public static IServiceCollection AddApplicationCore(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationContext>(options 
-            => options.UseSqlite(configuration.GetConnectionString("ClanCommanderSqlite")));
+        services.AddDbContext<ApplicationDbContext>(options 
+            => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
 
         services.AddClashOfClans(options =>
         {
             options.Tokens.Add(configuration["ClashOfClans:Token"]);
         });
 
-        // services.AddScoped<IMailService, MailService>();
-        services.AddScoped<IClashOfClansService, ClashOfClansService>();
-
         return services;
     }
 }
-
