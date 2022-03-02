0cdfd7e469580d9e7e32fa240a09e7345a4695a3
Jacob Ford <jaycub.k.ford@gmail.com>
2022-03-02 23:20:22 +0000

    feat: added joined guild client event handler

---
+++ src/ClanCommander.ApplicationCore/Features/Discord/Guilds/ClientEvents/JoinedDiscordGuildClientEvent.cs
@@ -0,0 +1,54 @@
+﻿namespace ClanCommander.ApplicationCore.Features.Discord.Guilds.ClientEvents;
+
+/// <summary>
+/// This event is for when the Bot joins a Discord Guild
+/// </summary>
+public class JoinedDiscordGuildClientEvent : INotification
+{
+    public ulong GuildId { get; set; }
+    public string GuildName { get; set; }
+    public ulong GuildOwner { get; set; }
+
+    public JoinedDiscordGuildClientEvent(ulong guildId, string guildName, ulong guildOwner)
+    {
+        GuildId = guildId;
+        GuildName = guildName;
+        GuildOwner = guildOwner;
+    }
+
+    internal class JoinedDiscordGuildClientEventHandler : INotificationHandler<JoinedDiscordGuildClientEvent>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public JoinedDiscordGuildClientEventHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task Handle(JoinedDiscordGuildClientEvent notification, CancellationToken cancellationToken)
+        {
+            await using var scope = _serviceProvider.CreateAsyncScope();
+            var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>()
+                ?? throw new NullReferenceException();
+
+            var guildAlreadyExists = await dbContext.DiscordGuilds.SingleOrDefaultAsync(x => x.GuildId == DiscordGuildId.FromUInt64(notification.GuildId));
+
+            if (guildAlreadyExists is not null)
+            {
+                guildAlreadyExists.UpdateGuildName(notification.GuildName);
+                guildAlreadyExists.ChangeOwner(DiscordUserId.FromUInt64(notification.GuildOwner));
+
+                await dbContext.SaveChangesAsync();
+                return;
+            }
+
+            var guild = new RegisteredDiscordGuild(
+                    DiscordGuildId.FromUInt64(notification.GuildId), 
+                    notification.GuildName, 
+                    DiscordUserId.FromUInt64(notification.GuildOwner));
+
+            await dbContext.AddAsync(guild);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ tests/ClanCommander.IntegrationTests/DiscordSocketClientEvents/JoinedDiscordGuildClientEventTests.cs
@@ -0,0 +1,47 @@
+﻿using ClanCommander.ApplicationCore.Features.Discord.Guilds.ClientEvents;
+
+namespace ClanCommander.IntegrationTests.DiscordSocketClientEvents;
+
+public class JoinedDiscordGuildClientEventTests : TestBase
+{
+    // Test Guild One is to test fully setup guilds
+    private readonly DiscordGuildId _testGuildOneId = DiscordGuildId.FromUInt64(760910445686161488u);
+    private readonly string _testGuildOneName = "Test1";
+    private readonly DiscordUserId _testGuildOneOwner = DiscordUserId.FromUInt64(339924145909399562u);
+
+    // Test Guild Two is to test guilds without message commands setup
+    private readonly DiscordGuildId _testGuildTwoId = DiscordGuildId.FromUInt64(834837883810349098u);
+    private readonly string _testGuildTwoName = "Test2";
+    private readonly DiscordUserId _testGuildTwoOwner = DiscordUserId.FromUInt64(751887766404726806u);
+    private readonly RegisteredDiscordGuild _testGuildTwo;
+
+    public JoinedDiscordGuildClientEventTests() : base()
+    {
+        _testGuildTwo = new RegisteredDiscordGuild(_testGuildTwoId, _testGuildTwoName, _testGuildTwoOwner);
+
+        this.SeedTestData();
+    }
+
+    [Fact]
+    public async void ShouldRegisterDiscordGuild_WhenBotJoinsGuild()
+    {
+        await Mediator.Publish(new JoinedDiscordGuildClientEvent(
+            _testGuildOneId.Value,
+            _testGuildOneName,
+            _testGuildOneOwner.Value));
+
+        var result = await ApplicationDbContext.DiscordGuilds.SingleOrDefaultAsync(x => x.GuildId == _testGuildOneId);
+        await ApplicationDbContext.Entry(result).ReloadAsync();
+
+        result.Should().NotBeNull();
+        result.GuildId.Should().Be(_testGuildOneId);
+        result.Name.Should().Be(_testGuildOneName);
+        result.OwnerId.Should().Be(_testGuildOneOwner);
+    }
+
+    private void SeedTestData()
+    {
+        ApplicationDbContext.DiscordGuilds.Add(_testGuildTwo);
+        ApplicationDbContext.SaveChanges();
+    }
+}
