480d088dc9673332d01f388f960f4a98ac985e27
Jacob Ford <jaycub.k.ford@gmail.com>
2022-03-02 23:21:01 +0000

    feat: added left guild client event handler

---
+++ src/ClanCommander.ApplicationCore/Features/Discord/Guilds/ClientEvents/LeftDiscordGuildClientEvent.cs
@@ -0,0 +1,39 @@
+﻿namespace ClanCommander.ApplicationCore.Features.Discord.Guilds.ClientEvents;
+
+/// <summary>
+/// This event is for when the Bot leaves a Discord Guild
+/// </summary>
+public class LeftDiscordGuildClientEvent : INotification
+{
+    public ulong GuildId { get; set; }
+
+    public LeftDiscordGuildClientEvent(ulong guildId)
+    {
+        GuildId = guildId;
+    }
+
+    internal class LeftDiscordGuildClientEventHandler : INotificationHandler<LeftDiscordGuildClientEvent>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public LeftDiscordGuildClientEventHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task Handle(LeftDiscordGuildClientEvent notification, CancellationToken cancellationToken)
+        {
+            await using var scope = _serviceProvider.CreateAsyncScope();
+            var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>()
+                ?? throw new NullReferenceException();
+
+            var guild = await dbContext.DiscordGuilds.SingleOrDefaultAsync(x => x.GuildId == DiscordGuildId.FromUInt64(notification.GuildId));
+
+            if (guild is not null)
+            {
+                dbContext.Remove(guild);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
