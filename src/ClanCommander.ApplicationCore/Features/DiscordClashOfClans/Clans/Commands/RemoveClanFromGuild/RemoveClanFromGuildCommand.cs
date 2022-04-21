ec5945711a9a9c8e1417e4e4685c5b2b5a229570
Jacob Ford <jaycub.k.ford@gmail.com>
2022-04-21 03:31:08 +0100

    feat: added RemoveClanFromGuild command

---
+++ src/ClanCommander.ApplicationCore/Features/DiscordClashOfClans/Clans/Commands/RemoveClanFromGuild/RemoveClanFromGuildCommand.cs
@@ -0,0 +1,44 @@
+﻿namespace ClanCommander.ApplicationCore.Features.DiscordClashOfClans.Clans.Commands.RemoveGuildClan;
+
+public class RemoveClanFromGuildCommand : IRequest
+{
+    public ulong GuildId { get; set; }
+
+    public string ClanId { get; set; }
+
+    public RemoveClanFromGuildCommand(ulong guildId, string clanId)
+    {
+        GuildId = guildId;
+        ClanId = clanId;
+    }
+
+    internal class RemoveClanFromGuildCommandHandler : IRequestHandler<RemoveClanFromGuildCommand>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public RemoveClanFromGuildCommandHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task<Unit> Handle(RemoveClanFromGuildCommand request, CancellationToken cancellationToken)
+        {
+            var requestClanId = Entities.ClashOfClans.ClanId.FromString(request.ClanId);
+            var requestGuildId = DiscordGuildId.FromUInt64(request.GuildId);
+
+            await using var scope = _serviceProvider.CreateAsyncScope();
+            var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>()
+                ?? throw new NullReferenceException();
+
+            var clan = await dbContext.GuildClans.FirstOrDefaultAsync(guildClan => guildClan.ClanId == requestClanId && guildClan.GuildId == requestGuildId, cancellationToken);
+            
+            if (clan is not null)
+            {
+                dbContext.Remove(clan);
+                await dbContext.SaveChangesAsync(cancellationToken);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ tests/ClanCommander.IntegrationTests/Commands/DiscordClashOfClans/Clans/RemoveClanFromGuildCommandTests.cs
@@ -0,0 +1,33 @@
+﻿namespace ClanCommander.IntegrationTests.Commands.DiscordClashOfClans.Clans;
+
+public class RemoveClanFromGuildCommandTests : TestBase
+{
+    [Fact]
+    public async void ShouldRemoveClanFromGuild()
+    {
+        // Arrange
+        var guildMock = new ValidRegisteredDiscordGuildMock();
+        await guildMock.SeedToDatabaseAsync(ServiceProvider);
+
+        var userMock = new ValidDiscordUserMock();
+        await userMock.SeedToDatabaseAsync(ServiceProvider);
+
+        var accountMock = new ValidClashOfClansAccountMock();
+        await accountMock.SeedToDatabaseAsync(ServiceProvider);
+
+        var clanMock = new ValidDiscordClanMock();
+        clanMock.WithValidClanMember();
+        await clanMock.SeedToDatabaseAsync(ServiceProvider);
+
+        // Act
+        var result = await Mediator.Send(new RemoveClanFromGuildCommand(clanMock.GuildId.Value, clanMock.ClanId.Value));
+
+        // Assert
+        await using var scope = ServiceProvider.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        var clan = await context.GuildClans.FirstOrDefaultAsync(x => x.GuildId == clanMock.GuildId && x.ClanId == clanMock.ClanId);
+
+        clan.Should().BeNull();
+    }
+}
