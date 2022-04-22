fc29611c645c2284f952d6bc782239f7caaf8aae
Jacob Ford <jaycub.k.ford@gmail.com>
2022-04-22 21:53:18 +0100

    tests: updated clash of clans account mock data

--- tests/ClanCommander.IntegrationTests/Mocks/ClashOfClansApiPlayerServiceMock.cs
+++ tests/ClanCommander.IntegrationTests/Mocks/ClashOfClansApiPlayerServiceMock.cs
@@ -11,8 +11,53 @@ public class ClashOfClansApiPlayerServiceMock : IClashOfClansApiPlayerService
         {
             Tag = "#PQU9QLP2V",
             Name = "JAY",
+            Role = ClashOfClans.Models.Role.Leader,
+            ExpLevel = 212,
+            Clan = new ClashOfClans.Models.PlayerClan
+            {
+                Tag = "#9UGQ0GL",
+                Name = "PlaneClashers",
+            },
             TownHallLevel = 14,
             TownHallWeaponLevel = 5,
+            Heroes = new ClashOfClans.Models.PlayerItemLevelList
+            {
+                new ClashOfClans.Models.PlayerItemLevel
+                {
+                    Name = "Barbarian King",
+                    Level = 80,
+                    MaxLevel = 80,
+                    Village = ClashOfClans.Models.Village.Home,
+                },
+                new ClashOfClans.Models.PlayerItemLevel
+                {
+                    Name = "Archer Queen",
+                    Level = 80,
+                    MaxLevel = 80,
+                    Village = ClashOfClans.Models.Village.Home,
+                },
+                new ClashOfClans.Models.PlayerItemLevel
+                {
+                    Name = "Garden Warden",
+                    Level = 55,
+                    MaxLevel = 55,
+                    Village = ClashOfClans.Models.Village.Home,
+                },
+                new ClashOfClans.Models.PlayerItemLevel
+                {
+                    Name = "Royal Champion",
+                    Level = 30,
+                    MaxLevel = 30,
+                    Village = ClashOfClans.Models.Village.Home,
+                },
+                new ClashOfClans.Models.PlayerItemLevel
+                {
+                    Name = "Master Builder",
+                    Level = 30,
+                    MaxLevel = 30,
+                    Village = ClashOfClans.Models.Village.Home,
+                },
+            }
         };
     }
 }
