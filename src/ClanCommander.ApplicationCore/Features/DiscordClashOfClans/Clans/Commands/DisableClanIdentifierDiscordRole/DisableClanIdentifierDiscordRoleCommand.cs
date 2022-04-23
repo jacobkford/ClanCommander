07df34b20830cb2db7254d5e4fd8292ce96d2f85
Jacob Ford <jaycub.k.ford@gmail.com>
2022-04-23 16:35:58 +0100

    feat: added disable clan discord role command

---
+++ src/ClanCommander.ApplicationCore/Features/DiscordClashOfClans/Clans/Commands/DisableClanIdentifierDiscordRole/DisableClanIdentifierDiscordRoleCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace ClanCommander.ApplicationCore.Features.DiscordClashOfClans.Clans.Commands.DisableClanIdentifierDiscordRole;
+
+public class DisableClanIdentifierDiscordRoleCommand : IRequest
+{
+    public ulong GuildId { get; private set; }
+
+    public string ClanId { get; private set; }
+
+    public DisableClanIdentifierDiscordRoleCommand(ulong guildId, string clanId)
+    {
+        GuildId = guildId;
+        ClanId = clanId;
+    }
+
+    internal class DisableClanIdentifierDiscordRoleCommandHandler : IRequestHandler<DisableClanIdentifierDiscordRoleCommand>
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DisableClanIdentifierDiscordRoleCommandHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task<Unit> Handle(DisableClanIdentifierDiscordRoleCommand request, CancellationToken cancellationToken)
+        {
+            var requestClanId = Entities.ClashOfClans.ClanId.FromString(request.ClanId);
+            var requestGuildId = DiscordGuildId.FromUInt64(request.GuildId);
+
+            await using var scope = _serviceProvider.CreateAsyncScope();
+            var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>()
+                ?? throw new NullReferenceException();
+
+            var clan = await dbContext.GuildClans.SingleOrDefaultAsync(x => x.ClanId == requestClanId && x.GuildId == requestGuildId, cancellationToken)
+                ?? throw new ArgumentException($"Clan with the Id of '{request.ClanId}' was not found.");
+
+            clan.DisableDiscordRole();
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
---
+++ tests/ClanCommander.IntegrationTests/Commands/DiscordClashOfClans/Clans/DisableClanIdentifierDiscordRoleCommandTests.cs
@@ -0,0 +1,32 @@
+﻿namespace ClanCommander.IntegrationTests.Commands.DiscordClashOfClans.Clans;
+
+public class DisableClanIdentifierDiscordRoleCommandTests : TestBase
+{
+    [Fact]
+    public async void ShouldDisableClanDiscordRole()
+    {
+        // Arrange
+        var guildMock = new ValidRegisteredDiscordGuildMock();
+        await guildMock.SeedToDatabaseAsync(ServiceProvider);
+
+        var userMock = new ValidDiscordUserMock();
+        await userMock.SeedToDatabaseAsync(ServiceProvider);
+
+        var accountMock = new ValidClashOfClansAccountMock();
+        await accountMock.SeedToDatabaseAsync(ServiceProvider);
+
+        var clanMock = new ValidDiscordClanMock();
+        await clanMock.SeedToDatabaseAsync(ServiceProvider);
+
+        // Act
+        await Mediator.Send(new DisableClanIdentifierDiscordRoleCommand(guildMock.GuildId.Value, clanMock.ClanId.Value));
+
+        // Assert
+        await using var scope = ServiceProvider.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        var clan = await context.GuildClans.FirstOrDefaultAsync(x => x.GuildId == clanMock.GuildId && x.ClanId == clanMock.ClanId);
+
+        clan?.DiscordRoleId.Should().Be(default);
+    }
+}
