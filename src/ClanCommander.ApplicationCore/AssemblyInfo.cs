f68c33066d86e89d4f57aa1aa2fe3054cad6cbc6
Jacob Ford <jaycub.k.ford@gmail.com>
2022-04-23 02:33:03 +0100

    feat: fixed namespaces

--- src/ClanCommander.ApplicationCore/AssemblyInfo.cs
+++ src/ClanCommander.ApplicationCore/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("ClanCommander.UnitTests")]
-[assembly: InternalsVisibleTo("ClanCommander.IntegrationTests")] 
+[assembly:InternalsVisibleTo("ClanCommander.IntegrationTests")] 
