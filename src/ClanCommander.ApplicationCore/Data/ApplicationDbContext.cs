7d12052af94382e7f92b9136e3da26667a686e42
Jacob Ford <jaycub.k.ford@gmail.com>
2022-04-23 01:12:24 +0100

    feat: added domain event publishing functionality

--- src/ClanCommander.ApplicationCore/Data/ApplicationDbContext.cs
+++ src/ClanCommander.ApplicationCore/Data/ApplicationDbContext.cs
@@ -16,13 +16,37 @@ internal class ApplicationDbContext : DbContext
     public DbSet<ClashOfClansAccount> ClashOfClansUserAccounts { get; set; }
     #endregion
 
-    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    private readonly IMediator _mediator;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public ApplicationDbContext(DbContextOptions options, IMediator mediator) : base(options) 
+    { 
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEntities = this.ChangeTracker.Entries<Entity>()
+            .Where(x => x.Entity.DomainEvents is not null && x.Entity.DomainEvents.Any());
+
+        var domainEvents = domainEntities.SelectMany(x => x.Entity.DomainEvents!).ToList();
+
+        domainEntities.ToList().ForEach(entity => entity.Entity.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await _mediator.Publish(domainEvent, cancellationToken);
+
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        return true;
     }
 }
 
--- src/ClanCommander.ApplicationCore/Data/ApplicationDbContextFactory.cs
+++ src/ClanCommander.ApplicationCore/Data/ApplicationDbContextFactory.cs
@@ -14,7 +14,40 @@ internal class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applica
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
         optionsBuilder.UseNpgsql(connectionString);
 
-        return new ApplicationDbContext(optionsBuilder.Options);
+        return new ApplicationDbContext(optionsBuilder.Options, new NoMediator());
 
+    }
+
+    class NoMediator : IMediator
+    {
+        public IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            return default;
+        }
+
+        public IAsyncEnumerable<object?> CreateStream(object request, CancellationToken cancellationToken = default)
+        {
+            return default;
+        }
+
+        public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default(CancellationToken)) where TNotification : INotification
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Publish(object notification, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult<TResponse>(default);
+        }
+
+        public Task<object?> Send(object request, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(default(object));
+        }
     }
 }
