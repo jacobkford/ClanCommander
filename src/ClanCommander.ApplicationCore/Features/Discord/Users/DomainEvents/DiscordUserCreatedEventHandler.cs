6a25e858d42d23550062471d1c90dd82df707ca7
Jacob Ford <jaycub.k.ford@gmail.com>
2022-04-23 02:32:01 +0100

    feat: added discord user created event handler

---
+++ src/ClanCommander.ApplicationCore/Features/Discord/Users/DomainEvents/DiscordUserCreatedEventHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace ClanCommander.ApplicationCore.Features.Discord.Users.DomainEvents;
+
+internal class DiscordUserCreatedEventHandler : INotificationHandler<DiscordUserCreatedEvent>
+{
+    public Task Handle(DiscordUserCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        // TODO: Send to log discord channel 
+        return Task.CompletedTask;
+    }
+}
