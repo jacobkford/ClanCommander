a3ba4537e764f6dab2b209d0f0c6e5e3ae1f6e0a
Jacob Ford <jaycub.k.ford@gmail.com>
2022-04-25 13:18:18 +0100

    fix: added owner attribute & updated global usings

---
+++ src/ClanCommander.DiscordBot/Preconditions/RequireBotOwnerAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace ClanCommander.DiscordBot.Preconditions;
+
+public class RequireBotOwnerAttribute : Discord.Commands.PreconditionAttribute
+{
+    public override Task<Discord.Commands.PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+    {
+        var configuration = services.GetService<IConfiguration>();
+        var ownerId = configuration.GetValue<ulong>("Discord:BotOwnerId");
+
+        if (ownerId == context.User.Id)
+            return Task.FromResult(Discord.Commands.PreconditionResult.FromSuccess());
+        else
+            return Task.FromResult(Discord.Commands.PreconditionResult.FromError("You must be the bot owner to run this command"));
+    }
+}
